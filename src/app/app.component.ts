77176e7b3ab4d617c212bd094fc4acb0ac6b0f5c
Hansel <hanselcapillo@hotmail.com>
2019-12-07 22:58:04 -0400

    Creación de componentes adicionales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'redux-app';
+  public contador: number;
+
+  constructor() {
+    this.contador = 10;
+  }
+
+  public incrementar() {
+    this.contador++;
+  }
+
+  public decrementar() {
+    this.contador--;
+  }
 }
---
+++ src/app/contador/hijo/hijo.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+
+@Component({
+  selector: "app-hijo",
+  templateUrl: "./hijo.component.html",
+  styles: [
+    `
+      button {
+        margin: 10px;
+      }
+    `
+  ]
+})
+export class HijoComponent implements OnInit {
+  @Input() contador: number;
+  @Output() cambioContador = new EventEmitter<number>();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  public multiplicar() {
+    this.contador *= 2;
+    this.cambioContador.emit(this.contador);
+  }
+
+  public dividir() {
+    this.contador /= 2;
+    this.cambioContador.emit(this.contador);
+  }
+}
