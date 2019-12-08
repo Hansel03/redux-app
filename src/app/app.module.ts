b7c20b522f09bc22e7a68c9244ca2b5c17560aef
Hansel <hanselcapillo@hotmail.com>
2019-12-08 16:34:54 -0400

    Store DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -7,13 +8,18 @@ import { HijoComponent } from "./contador/hijo/hijo.component";
 import { NietoComponent } from "./contador/nieto/nieto.component";
 import { StoreModule } from "@ngrx/store";
 import { contadorReducer } from "./contador/contador.reducer";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [AppComponent, HijoComponent, NietoComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ contador: contadorReducer })
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Mantener las ultimas 25 acciones
+      logOnly: environment.production // Restrict extension to log-only mode
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
