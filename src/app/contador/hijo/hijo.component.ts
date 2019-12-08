66b2ab2c382db5c788dc270d2c198add01068511
Hansel <hanselcapillo@hotmail.com>
2019-12-07 23:18:45 -0400

    Integrar el componente nieto en nuestra aplicación

--- src/app/contador/hijo/hijo.component.ts
+++ src/app/contador/hijo/hijo.component.ts
@@ -28,4 +28,9 @@ export class HijoComponent implements OnInit {
     this.contador /= 2;
     this.cambioContador.emit(this.contador);
   }
+
+  public resetNieto(nuevoContador) {
+    this.contador = nuevoContador;
+    this.cambioContador.emit(this.contador);
+  }
 }
--- src/app/contador/nieto/nieto.component.ts
+++ src/app/contador/nieto/nieto.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-nieto',
-  templateUrl: './nieto.component.html',
+  selector: "app-nieto",
+  templateUrl: "./nieto.component.html",
   styles: []
 })
 export class NietoComponent implements OnInit {
+  @Input() contador: number;
+  @Output() resetContador = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public reset() {
+    this.resetContador.emit(0);
   }
-
 }
