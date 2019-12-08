e38365ea551f013534594b93bd1d292b58fdc231
Hansel <hanselcapillo@hotmail.com>
2019-12-08 16:14:11 -0400

    Action Creator - Creador de acciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
-import { Store, Action } from "@ngrx/store";
+import { Store } from "@ngrx/store";
+import {
+  IncrementarAction,
+  DecrementarAction
+} from "./contador/contador.actions";
 
 interface AppState {
   contador: number;
@@ -21,17 +25,21 @@ export class AppComponent {
 
   public incrementar() {
     // this.contador++;
-    const accion: Action = {
-      type: "INCREMENTAR"
-    };
+    // const accion: Action = {
+    //   type: "INCREMENTAR"
+    // };
+
+    const accion = new IncrementarAction();
     this.store.dispatch(accion);
   }
 
   public decrementar() {
     // this.contador--;
-    const accion: Action = {
-      type: "DECREMENTAR"
-    };
+    // const accion: Action = {
+    //   type: "DECREMENTAR"
+    // };
+
+    const accion = new DecrementarAction();
     this.store.dispatch(accion);
   }
 }
